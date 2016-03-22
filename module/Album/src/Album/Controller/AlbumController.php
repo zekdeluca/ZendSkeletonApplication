2d77cea3e5d6314e2727d84d103bbbf09a992894
zekdeluca <ezequiel_deluca@yahoo.com>
2016-03-22 10:55:20 -0400

    Basic CRUD for Album

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -4,6 +4,9 @@ namespace Album\Controller;
 
  use Zend\Mvc\Controller\AbstractActionController;
  use Zend\View\Model\ViewModel;
+ // For the use of the form and input validation
+ use Album\Model\Album;
+ use Album\Form\AlbumForm;
 
  class AlbumController extends AbstractActionController
  {
@@ -19,14 +22,95 @@ namespace Album\Controller;
 
      public function addAction()
      {
+         $form = new AlbumForm();
+         $form->get('submit')->setValue('Add');
+
+         $request = $this->getRequest();
+
+         if ($request->isPost()) {
+             $album = new Album();
+             $form->setInputFilter($album->getInputFilter());
+             $form->setData($request->getPost());
+
+             if ($form->isValid()) {
+                 $album->exchangeArray($form->getData());
+                 $this->getAlbumTable()->saveAlbum($album);
+
+                 // Redirect to list of albums
+                 return $this->redirect()->toRoute('album');
+             }
+         }
+
+         return array('form' => $form);
      }
 
      public function editAction()
      {
+         $id = (int) $this->params()->fromRoute('id', 0);
+         if (!$id) {
+             return $this->redirect()->toRoute('album', array(
+                 'action' => 'add'
+             ));
+         }
+
+         // Get the Album with the specified id.  An exception is thrown
+         // if it cannot be found, in which case go to the index page.
+         try {
+             $album = $this->getAlbumTable()->getAlbum($id);
+         }
+         catch (\Exception $ex) {
+             return $this->redirect()->toRoute('album', array(
+                 'action' => 'index'
+             ));
+         }
+
+         $form  = new AlbumForm();
+         $form->bind($album);
+         $form->get('submit')->setAttribute('value', 'Edit');
+
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+             $form->setInputFilter($album->getInputFilter());
+             $form->setData($request->getPost());
+
+             if ($form->isValid()) {
+                 $this->getAlbumTable()->saveAlbum($album);
+
+                 // Redirect to list of albums
+                 return $this->redirect()->toRoute('album');
+             }
+         }
+
+         return array(
+             'id' => $id,
+             'form' => $form,
+         );
      }
 
      public function deleteAction()
      {
+         $id = (int) $this->params()->fromRoute('id', 0);
+         if (!$id) {
+             return $this->redirect()->toRoute('album');
+         }
+
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+             $del = $request->getPost('del', 'No');
+
+             if ($del == 'Yes') {
+                 $id = (int) $request->getPost('id');
+                 $this->getAlbumTable()->deleteAlbum($id);
+             }
+
+             // Redirect to list of albums
+             return $this->redirect()->toRoute('album');
+         }
+
+         return array(
+             'id'    => $id,
+             'album' => $this->getAlbumTable()->getAlbum($id)
+         );
      }
 
      public function getAlbumTable()
@@ -35,6 +119,7 @@ namespace Album\Controller;
              $sm = $this->getServiceLocator();
              $this->albumTable = $sm->get('Album\Model\AlbumTable');
          }
+
          return $this->albumTable;
      }
  }
--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -2,16 +2,92 @@
 
 namespace Album\Model;
 
- class Album
+// Classes used for input filtering
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+ class Album implements InputFilterAwareInterface
  {
      public $id;
      public $artist;
      public $title;
 
+     protected $inputFilter;
+
+     // Method required for the use of the Zend\Stdlib\Hydrator\ArraySerializable hydrator
      public function exchangeArray($data)
      {
          $this->id     = (!empty($data['id'])) ? $data['id'] : null;
          $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
          $this->title  = (!empty($data['title'])) ? $data['title'] : null;
+     }
+
+     // Method required for the use of the Zend\Stdlib\Hydrator\ArraySerializable hydrator
+     public function getArrayCopy()
+     {
+         return get_object_vars($this);
+     }
+
+     public function setInputFilter(InputFilterInterface $inputFilter)
+     {
+         throw new \Exception("Not used");
+     }
+
+     public function getInputFilter()
+     {
+         if (!$this->inputFilter) {
+             $inputFilter = new InputFilter();
+
+             $inputFilter->add(array(
+                 'name'     => 'id',
+                 'required' => true,
+                 'filters'  => array(
+                     array('name' => 'Int'),
+                 ),
+             ));
+
+             $inputFilter->add(array(
+                 'name'     => 'artist',
+                 'required' => true,
+                 'filters'  => array(
+                     array('name' => 'StripTags'),
+                     array('name' => 'StringTrim'),
+                 ),
+                 'validators' => array(
+                     array(
+                         'name'    => 'StringLength',
+                         'options' => array(
+                             'encoding' => 'UTF-8',
+                             'min'      => 1,
+                             'max'      => 100,
+                         ),
+                     ),
+                 ),
+             ));
+
+             $inputFilter->add(array(
+                 'name'     => 'title',
+                 'required' => true,
+                 'filters'  => array(
+                     array('name' => 'StripTags'),
+                     array('name' => 'StringTrim'),
+                 ),
+                 'validators' => array(
+                     array(
+                         'name'    => 'StringLength',
+                         'options' => array(
+                             'encoding' => 'UTF-8',
+                             'min'      => 1,
+                             'max'      => 100,
+                         ),
+                     ),
+                 ),
+             ));
+
+             $this->inputFilter = $inputFilter;
+         }
+
+         return $this->inputFilter;
      }
  }
